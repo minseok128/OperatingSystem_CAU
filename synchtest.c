c75a088c61596a7c1dd8495faa182b2d832b7b84
minseok128 <minseok128128@gmail.com>
2023-05-30 18:41:27 +0900

    test1

--- synchtest.c
+++ synchtest.c
@@ -183,10 +183,8 @@ message_function(int car_num, int start, int before, int current, int after, int
 
 	kprintf("car = %d, in = %s, out = %s : %s->%s", car_num, get_direction_by_num(before), get_direction_by_num(destination), get_direction_by_num(before), get_direction_by_num(current));
 	if (current == destination)
-		kprintf("car: %d, arrive %s, start: %s", car_num, get_direction_by_num(destination), get_direction_by_num(start));
-	else
-		kprintf("car: %d, moved %s -> %s, start: %s, after: %s, dest: %s", car_num, get_direction_by_num(before), get_direction_by_num(current), get_direction_by_num(start), get_direction_by_num(after), get_direction_by_num(destination));
-	kprintf("\n\n");
+		kprintf(" done");
+	kprintf("\n\n\n");
 	V(donesem);
 }
 
