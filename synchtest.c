ede0b04f3bf52d5acd293676f667c66a9778cc56
minseok128 <minseok128128@gmail.com>
2023-05-30 20:58:08 +0900

    fix bug

--- synchtest.c
+++ synchtest.c
@@ -250,7 +250,7 @@ semtestthread(void *junk, unsigned long car_num)
 	(void)junk;
 
 	start_num = end_num = random() % 4;
-	while (start_num == end_num && start_num == (end_num + 3) % 4)
+	while (start_num == end_num || start_num == (end_num + 3) % 4)
 		end_num = random() % 4;
 	/*
 	 * Only one of these should print at a time.
