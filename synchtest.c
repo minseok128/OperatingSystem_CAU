4b59799182fdb2d8e953e39afad4ce65de7129ff
minseok128 <minseok128128@gmail.com>
2023-05-30 17:39:53 +0900

    줄이기

--- synchtest.c
+++ synchtest.c
@@ -41,7 +41,7 @@
 #define NSEMLOOPS 63
 #define NLOCKLOOPS 120
 #define NCVLOOPS 5
-#define NTHREADS 32
+#define NTHREADS 8
 
 static volatile unsigned long testval1;
 static volatile unsigned long testval2;
